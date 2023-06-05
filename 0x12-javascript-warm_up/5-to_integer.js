13b48dbd5995242f42254448eea0f381092172b7
oluwaseunT <taiwooluwaseun658@gmail.com>
2023-06-05 07:38:48 +0000

    my first javascript commit

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,7 @@
+t process = require('process');
+const num = parseInt(process.argv[2]);
+if (num) {
+  console.log('My number:', num);
+} else {
+  console.log('Not a number');
+}
---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,6 @@
+t lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+let x = 0;
+while (x < 3) {
+  console.log(lines[x]);
+  x++;
+}
