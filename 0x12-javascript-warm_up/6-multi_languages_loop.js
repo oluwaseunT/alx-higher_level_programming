cba08c34a9ba36ee8e24c3cf8982924fa610b922
oluwaseunT <taiwooluwaseun658@gmail.com>
2023-06-05 07:56:06 +0000

    bug fixed @ file 6

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,4 +1,5 @@
-t lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+#!/usr/bin/node
+const lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 let x = 0;
 while (x < 3) {
   console.log(lines[x]);
